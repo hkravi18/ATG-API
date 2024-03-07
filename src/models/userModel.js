e3b63b5946c568e17e1ba2a19b14ed36365df01e
harshit kumar ravi <hkravi2002@gmail.com>
2024-03-07 15:41:56 +0530

    Added user model in mongodb database using mongoose ORM

---
+++ src/models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
